4d3268ae87b75a49c92d293a2111278dbc6cc0e5
RISHABH MISHRA <rish.mish.2607@gmail.com>
2021-09-24 14:16:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+var readlineSync = require("readline-sync");
+var score = 0;
+console.log("Hello");
+var userName = readlineSync.question("What is your Name? ");
+console.log("Welcome "+ userName + " to the quiz");
+var questionAnswerList = [
+  {question:"Who is president of the USA ? ",
+  answer:"Joe Biden"},
+   {question:"Capital of karnataka is ? ",
+  answer:"Bangalore"},
+   {question:"Full form of BSE is ? ",
+  answer:"Bombay Stock Exchange"},
+   {question:"2+2? ",
+  answer:"22"},
+   {question:"Color of tajmahal is? ",
+  answer:"yellow"},
+   {question:"",
+  answer:""},
+
+];
+
+var highScore = {
+  name:"Rishabh",
+  score:5
+};
+
+function askQuestion(question, answer){
+  var userAns = readlineSync.question(question);
+  if(userAns === answer){
+    console.log("You are right ");
+    score++;
+  }else{
+    console.log("You are wrong ");
+  }
+  console.log("Your score is "+ score);
+  console.log("-------");
+};
+
+
+for (var i=0; i<questionAnswerList.length-1; i++){
+  askQuestion(questionAnswerList[i].question, questionAnswerList[i].answer);
+}
+console.log("Your final score is "+ score);
+if(score === highScore.score){
+  console.log("You have equalled highscore");
+}
+
+console.log("Thankyou for playing");
